c65ff6661801e1de8adef1bf9c578df877fcff8c
RRegnieRR <joseregnier14@gmail.com>
2026-05-20 17:45:00 -0600

    .

--- week3/script/course.mjs
+++ week3/script/course.mjs
@@ -1,8 +1,35 @@
-sections.forEach((section) => {
-    const option = document.createElement("option");
-    option.value = section.sectionNumber;
-    option.textContent = `${section.sectionNumber}`;
-    sectionSelect.appendChild(option);
-  });
+const byuiCourse = {
+  code: "WDD231",
+  name: "Web Frontend Development I",
+  sections: [
+    {
+      sectionNumber: 1,
+      enrolled: 88,
+      instructor: "Brother Bingham",
+    },
+    {
+      sectionNumber: 2,
+      enrolled: 81,
+      instructor: "Sister Shultz",
+    },
+    {
+      sectionNumber: 3,
+      enrolled: 95,
+      instructor: "Sister Smith",
+    },
+  ],
+  changeEnrollment: function (sectionNumber, add = true) {
+    const sectionIndex = this.sections.findIndex(
+      (section) => section.sectionNumber == sectionNumber
+    );
+    if (sectionIndex >= 0) {
+      if (add) {
+        this.sections[sectionIndex].enrolled++;
+      } else {
+        this.sections[sectionIndex].enrolled--;
+      }
+    }
+  },
+};
 
-  export default byuiCourse;
+export default byuiCourse;
