471401272e0a25bd054d7e22f54533c357fb425c
RRegnieRR <joseregnier14@gmail.com>
2026-05-21 20:04:17 -0600

    .

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -5,6 +5,15 @@ const spotlights = document.querySelector("#spotlights");
 const gridButton = document.querySelector("#grid");
 const listButton = document.querySelector("#list");
 const membersUrl = "data/members.json";
+const currentTemp = document.querySelector("#current-temp");
+const weatherDesc = document.querySelector("#weather-desc");
+const forecast = document.querySelector("#forecast");
+const weatherIcon = document.querySelector("#weather-icon");
+const myKey = "99eef65b0994f68f9446e9fd8ae5ec1e";
+const myLat = "20.6597";
+const myLon = "-103.3496";
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=metric`;
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=metric`;
 
 document.querySelector("#year").textContent = new Date().getFullYear();
 document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
@@ -91,13 +100,15 @@ function displaySpotlights(memberList) {
         name.textContent = member.name;
         phone.textContent = member.phone;
         address.textContent = member.address;
-        website.textContent = member.website;
+        website.textContent = `URL: ${member.website}`;
         website.setAttribute("href", member.website);
         website.setAttribute("target", "_blank");
         website.setAttribute("rel", "noopener");
+        phone.textContent = `PHONE: ${member.phone}`;
+        address.textContent = `ADDRESS: ${member.address}`;
         level.textContent = `${getMembershipName(member.membership)} Member`;
 
-        card.append(logo, name, phone, address, website, level);
+        card.append(name, level, logo, address, phone, website);
         spotlights.appendChild(card);
     });
 }
@@ -133,3 +144,45 @@ if (gridButton && listButton) {
 }
 
 getMembers();
+
+if (currentTemp) {
+    getWeather();
+}
+
+async function getWeather() {
+    try {
+        const response = await fetch(weatherUrl);
+        const data = await response.json();
+        currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;C`;
+        weatherDesc.textContent = data.weather[0].description;
+        const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        weatherIcon.setAttribute("src", iconsrc);
+        weatherIcon.setAttribute("alt", data.weather[0].description);
+    } catch (error) {
+        console.log(error);
+    }
+
+    try {
+        const response = await fetch(forecastUrl);
+        const data = await response.json();
+        displayForecast(data.list);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayForecast(weatherList) {
+    let daysShown = 0;
+
+    weatherList.forEach((item) => {
+        if (item.dt_txt.includes("12:00:00") && daysShown < 3) {
+            let p = document.createElement("p");
+            let date = new Date(item.dt_txt);
+            let day = date.toLocaleDateString("en-US", { weekday: "short" });
+
+            p.innerHTML = `${day}: <strong>${item.main.temp.toFixed(0)}&deg;C</strong>`;
+            forecast.appendChild(p);
+            daysShown++;
+        }
+    });
+}
